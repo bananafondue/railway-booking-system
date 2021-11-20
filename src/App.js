0ea889d7ad7ef1f5aa05f04d19ad431ce54d64eb
ShamikG17 <shamikghadge.17@gmail.com>
2021-11-20 14:07:01 +0530

    Add Home page and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Home from "./components/Home";
+import Listing from "./components/Listing";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listing" element={<Listing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <>
-      <p>Home</p>
+      <div className="search-form m-auto align-middle">
+        <div className="form-input">
+          <label htmlFor="location">From</label>
+          <input className="input" type="text" />
+        </div>
+        <i class="fas fa-exchange-alt d-flex align-self-center"></i>
+        <div className="form-input">
+          <label htmlFor="destination">To</label>
+          <input className="input" type="text" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="date">Departure Date</label>
+          <input className="input" type="date" />
+        </div>
+        <button className="search-btn d-flex align-self-center">
+          Search <i class="fas fa-search"></i>
+        </button>
+      </div>
     </>
   );
 };
